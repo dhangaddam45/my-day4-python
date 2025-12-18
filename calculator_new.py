0573c3bf2972bae4cbfbb9ae98b7acc47ec4f470
Dhana <dhanagaddam45@gmail.com>
2025-12-18 11:37:06 -0600

    added sys  command line argument to the calculator program

--- calculator_new.py
+++ calculator_new.py
@@ -1,5 +1,8 @@
 
 
+import sys
+
+
 def addition(num1, num2):
     add = num1 + num2
     return add
@@ -12,7 +15,22 @@ def mul(num1, num2):
     mul = num1 * num2
     return mul
 
-print(addition(5, 10))
-print(sub(10, 5))
-print(mul(10, 5))
-    
+print(addition(5, 10)) #
+print(sub(10, 5)) #
+print(mul(10, 5)) #
+
+if len(sys.argv) < 4:
+    print("Usage: python calculator_new.py num1 operation num2")
+else:
+    num1 = int(sys.argv[1])
+    operation = sys.argv[2]
+    num2 = int(sys.argv[3])
+    if operation == 'add':
+        result = addition(num1, num2)
+    elif operation == 'sub':
+        result = sub(num1, num2)
+    elif operation == 'mul':
+        result = mul(num1, num2)
+    else:
+        result = "Invalid operation"
+    print(f"Result: {result}")
